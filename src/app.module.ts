7bede76bad26d4842a3c2bd0235ace245340b68d
Junior <eecfredes@gmail.com>
2024-02-04 17:49:26 -0300

    :rocket: creating Query

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { UserResolver } from './graphql/resolvers/UserResolver';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: true,
     }),
   ],
   controllers: [],
-  providers: [],
+  providers: [UserResolver],
 })
 export class AppModule {}
