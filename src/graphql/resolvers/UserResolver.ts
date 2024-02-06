b807ad917248ec11cae6e4afd77284b02b3c9672
Junior <eecfredes@gmail.com>
2024-02-05 23:30:34 -0300

    :rocket: Query arguments

--- src/graphql/resolvers/UserResolver.ts
+++ src/graphql/resolvers/UserResolver.ts
@@ -1,14 +1,16 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args, Int } from '@nestjs/graphql';
 import { User } from '../models/User';
+import { mockUsers } from '../../__mocks__/mockUsers';
 
 @Resolver()
 export class UserResolver {
-  @Query((returns) => User)
-  getUseById() {
-    return {
-      id: 1,
-      username: 'John Doe',
-      displayName: 'John the gamer',
-    };
+  @Query((returns) => User, { nullable: true })
+  getUseById(@Args('id', { type: () => Int }) id: number) {
+    return mockUsers.find((user) => user.id === id);
+  }
+
+  @Query(() => [User])
+  getUsers() {
+    return mockUsers;
   }
 }
