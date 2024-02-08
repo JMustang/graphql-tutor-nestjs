362a961801a1743b8081e8105f623e5ff975b54e
Junior <eecfredes@gmail.com>
2024-02-07 22:22:31 -0300

    :rocket: UserSetting mock

---
+++ src/__mocks__/mockUserSettings.ts
@@ -0,0 +1,12 @@
+export const mockUserSettings = [
+  {
+    userId: 1,
+    receiveNotifications: false,
+    receiveEmail: false,
+  },
+  {
+    userId: 3,
+    receiveNotifications: true,
+    receiveEmail: false,
+  },
+];
