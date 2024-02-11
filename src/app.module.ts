06ca7027f148e884d3fb32756e8e8004bb107a91
Junior <eecfredes@gmail.com>
2024-02-11 18:02:32 -0300

    :rocket: Database: MySQL2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserResolver } from './graphql/resolvers/UserResolver';
 import { UserSettingsResolver } from './graphql/resolvers/UserSettingsResolver';
 
@@ -9,6 +10,16 @@ import { UserSettingsResolver } from './graphql/resolvers/UserSettingsResolver';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'testuser',
+      password: 'testuser',
+      database: 'graphql_tutorial',
+      entities: [],
+      synchronize: true,
     }),
   ],
   controllers: [],
